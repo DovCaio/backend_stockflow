a94de4b994c8b4f41584c08de42df4c9aee86451
DovCaio <onlydeadnotalive@gmail.com>
2025-08-03 22:27:48 -0300

    fix: conserte o erro do prisma com o container.

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from 'src/models/Product';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
 
+    constructor( private readonly prisma: PrismaService){}
 
-    public saveAProducts(product : Product){
+    public saveAProducts(product : Product) : Promise<Product | null>{
 
-        
+        return this.prisma.product.create({
+            data: product
+        })
 
     }
 
