6584480ca88a76c1fb2616b9a24d6328ec2e5c67
Matheus Victor <matheusvictor.dev@gmail.com>
2025-09-11 17:57:35 -0300

    feat: adiona middleware para evitar problemas de cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.useGlobalPipes(
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true, 
+      transform: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
