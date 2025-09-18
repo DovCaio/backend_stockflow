b85b919d4c05b2720935dda6cfc6c2c83e1ef700
DovCaio <onlydeadnotalive@gmail.com>
2025-09-17 22:37:01 -0300

    feat: altera mais uma vez o texto

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'O backend está rodando.';
+    return 'O backend está rodando';
   }
 }
